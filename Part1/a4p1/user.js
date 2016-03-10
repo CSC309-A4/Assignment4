18ea4f2b194fe6dc350abfb695b1f3ee16a1a0a6
c3bacals <peterkarlca@gmail.com>
2016-03-09 22:08:17 -0500

    added user signup and login

---
+++ Part1/a4p1/user.js
@@ -0,0 +1,31 @@
+// pretty cool effect
+$('.form').find('input, textarea').on('keyup', function (e) {
+  
+  var $this = $(this),
+      label = $this.prev('label');
+
+    if (e.type === 'keyup') {
+      if ($this.val() === '') {
+          label.removeClass('active highlight');
+        } else {
+          label.addClass('active highlight');
+        }
+    }
+
+});
+
+
+// toggle between sign up or login
+$('.tab a').on('click', function (e) {
+  
+  
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+  
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+  
+  $(target).fadeIn(600);
+  
+});

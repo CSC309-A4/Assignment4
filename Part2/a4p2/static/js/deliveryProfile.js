2e6f98d67674f5f931b313f500957e55e151cace
c3bacals <peterkarlca@gmail.com>
2016-04-06 22:41:42 -0400

    added helper functions; attepted ajax transition of orders but no luck

--- Part2/a4p2/static/js/deliveryProfile.js
+++ Part2/a4p2/static/js/deliveryProfile.js
@@ -28,11 +28,100 @@ $.ajax({
 
 		delivererInfo.html(html);
 		thisUser = data;
-		
 		updateOrderHistory();
 		
 	}
 });
+
+
+search.click(function (e){
+	var search_orders = $("#order-selection option:selected").text();
+	// filter by store name
+	if (search_orders == "Store Name"){
+		$.ajax({
+		type: "GET",
+		url: "search_store",
+		data: search_orders,
+		success: function(data, textStatus, jqXHR) {
+			var html = "";
+			// console.log(data);
+			for (i = 0; i < data.length; i++){
+				if (data[i].orderStatus == "Pending"){
+					html += orderLayout(data[i]);
+				}
+			}
+			displayOrders.html(html);
+
+			// Deliverer accepted an order
+			acceptOrder();
+
+			},
+		});
+	}
+	// filter order list according to city
+	else if (search_orders == "Near Me"){
+		var cur_location = thisUser.city;
+		$.ajax({
+		type: "GET",
+		url: "search_all",
+		data: search_orders,
+		success: function(data, textStatus, jqXHR) {
+			var html = "";
+			// console.log(data);
+			for (i = 0; i < data.length; i++){
+				if (data[i].orderStatus == "Pending"){
+					if (data[i].city == cur_location){
+						html += orderLayout(data[i]);
+					}
+				}
+			}
+			displayOrders.html(html);
+
+			// Deliverer accepted an order
+			acceptOrder();
+
+			},
+		});
+	}
+	// display all existing orders that are pending a deliverer
+	else if (search_orders == "List All"){
+		$.ajax({
+		type: "GET",
+		url: "search_all",
+		data: search_orders,
+		success: function(data, textStatus, jqXHR) {
+			var html = "";
+			// console.log(data);
+			for (i = 0; i < data.length; i++){
+				if (data[i].orderStatus == "Pending"){
+					html += orderLayout(data[i]);
+				}
+			}
+			displayOrders.html(html);
+			
+			// Deliverer accepted an order
+			acceptOrder();
+
+			},
+		});
+	}
+});
+
+// logout of profile
+logout.click(function (e) {
+	delete_cookie("loginDeliverer");
+	window.location = "index.html";
+});
+
+// Function that deletes a cookie.
+var delete_cookie = function(name) {
+    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+};
+
+
+/* --------------------------------------------
+	        HELPER FUNCTIONS 
+------------------------------------------*/
 
 function updateOrderHistory() {
 	$.ajax({
@@ -44,23 +133,23 @@ function updateOrderHistory() {
 			// console.log(data);
 			for (i = 0; i < data.length; i++){
 				html += "<form id='" + data[i]._id + "''>";
-					html += "<label for='orderID'>Order ID:</label>" 
-					        + "<input type='text' name='id' readonly='readonly' value='" + data[i]._id + "'><br>";
+				html += "<label for='orderID'>Order ID:</label>" 
+				        + "<input type='text' name='id' readonly='readonly' value='" + data[i]._id + "'><br>";
 
-					html += "<label for='deliveryLocation'>Delivery Location:</label>"
-							+ "<input type='text' name='location' readonly='readonly' value='" + data[i].userLocation + "'><br>";
+				html += "<label for='deliveryLocation'>Delivery Location:</label>"
+						+ "<input type='text' name='location' readonly='readonly' value='" + data[i].userLocation + "'><br>";
 
-					html += "<label for='store'>Store requested from:</label>"
-							+ "<input type='text' name='store' readonly='readonly' value='" + data[i].store + "'><br>";
+				html += "<label for='store'>Store requested from:</label>"
+						+ "<input type='text' name='store' readonly='readonly' value='" + data[i].store + "'><br>";
 
-					html += "<label for='food'>Food requested:</label>"
-							+ "<input type='text' name='food' readonly='readonly' value='" + data[i].food + "'><br>";
+				html += "<label for='food'>Food requested:</label>"
+						+ "<input type='text' name='food' readonly='readonly' value='" + data[i].food + "'><br>";
 
-					html += "<label for='date'>Date made:</label>"
-						 	+ "<input type='text' name='date' readonly='readonly' value='" + data[i].date + "'><br>";
+				html += "<label for='date'>Date made:</label>"
+					 	+ "<input type='text' name='date' readonly='readonly' value='" + data[i].date + "'><br>";
 
-					html += "<button type='submit' class='button button-block'>Cancel Order</button><br><br><br>";
-					html += "</form>";
+				html += "<button type='submit' class='button button-block'>Cancel Order</button><br><br><br>";
+				html += "</form>";
 			}
 			myOrders.html(html);
 			
@@ -72,6 +161,8 @@ function updateOrderHistory() {
 					var formData = {
 						orderID: $(this).find('input[name=id]').val(),
 					}
+
+					$(this).remove();
 
 			        $.ajax({
 			            type: 'post',
@@ -89,194 +180,56 @@ function updateOrderHistory() {
 			myOrders.html("Fetching failed");
 	  }
 	});
+	return false;
 }
 
-search.click(function (e){
-	var search_orders = $("#order-selection option:selected").text();
-	if (search_orders == "Store Name"){
-		$.ajax({
-		type: "GET",
-		url: "search_store",
-		data: search_orders,
-		success: function(data, textStatus, jqXHR) {
-			var html = "";
-			// console.log(data);
-			for (i = 0; i < data.length; i++){
-				if (data[i].orderStatus == "Pending"){
-					html += "<form id='" + data[i]._id + "''>";
-					html += "<label for='orderID'>Order ID:</label>" 
-					        + "<input type='text' name='id' readonly='readonly' value='" + data[i]._id + "'><br>";
+function acceptOrder() {
+	// Deliverer accepted an order
+	$(function () {
+	    $('form').on('submit', function (e) {
 
-					html += "<label for='deliveryLocation'>Delivery Location:</label>"
-							+ "<input type='text' name='location' readonly='readonly' value='" + data[i].userLocation + "'><br>";
-
-					html += "<label for='store'>Store requested from:</label>"
-							+ "<input type='text' name='store' readonly='readonly' value='" + data[i].store + "'><br>";
-
-					html += "<label for='food'>Food requested:</label>"
-							+ "<input type='text' name='food' readonly='readonly' value='" + data[i].food + "'><br>";
-
-					html += "<label for='date'>Date made:</label>"
-						 	+ "<input type='text' name='date' readonly='readonly' value='" + data[i].date + "'><br>";
-
-					html += "<button type='submit' class='button button-block'>Accept Order</button><br><br><br>";
-					html += "</form>";
-				}
+	    	console.log($(this).find('input[name=id]').val());
+			console.log(thisUser);
+			var formData = {
+				orderID: $(this).find('input[name=id]').val(),
+				deliverer: thisUser._id,
 			}
-			displayOrders.html(html);
 
-			// Deliverer accepted an order
-			$(function () {
-			    $('form').on('submit', function (e) {
+			$(this).remove();
 
-			    	console.log($(this).find('input[name=id]').val());
+	        $.ajax({
+	            type: 'post',
+	            url: 'update_order',
+	            data: formData,
+	            success: function () {
+	            	alert("Order accepted! Refresh the page to see the orders you accepted");
+	            	// return updateOrderHistory();
+	            }
+	        });
+	        e.preventDefault();
+	    });
+	});
+}
 
-					var formData = {
-						orderID: $(this).find('input[name=id]').val(),
-					}
+function orderLayout(data) {
+	html = '';
+	html += "<form id='" + data._id + "''>";
+	html += "<label for='orderID'>Order ID:</label>" 
+	        + "<input type='text' name='id' readonly='readonly' value='" + data._id + "'><br>";
 
-			        $.ajax({
-			            type: 'post',
-			            url: 'update_order',
-			            data: formData,
-			            success: function () {
-			            	alert("Order accepted!");
-			            }
-			        });
-			        e.preventDefault();
-			    });
-			});
-			},
-		});
-	}
-	else if (search_orders == "Near Me"){
-		var cur_location = thisUser.city;
-		$.ajax({
-		type: "GET",
-		url: "search_all",
-		data: search_orders,
-		success: function(data, textStatus, jqXHR) {
-			var html = "";
-			// console.log(data);
-			for (i = 0; i < data.length; i++){
-				if (data[i].orderStatus == "Pending"){
-					if (data[i].city == cur_location){
-						html += "<form id='" + data[i]._id + "''>";
-						html += "<label for='orderID'>Order ID:</label>" 
-						        + "<input type='text' name='id' readonly='readonly' value='" + data[i]._id + "'><br>";
+	html += "<label for='deliveryLocation'>Delivery Location:</label>"
+			+ "<input type='text' name='location' readonly='readonly' value='" + data.userLocation + "'><br>";
 
-						html += "<label for='deliveryLocation'>Delivery Location:</label>"
-								+ "<input type='text' name='location' readonly='readonly' value='" + data[i].userLocation + "'><br>";
+	html += "<label for='store'>Store requested from:</label>"
+			+ "<input type='text' name='store' readonly='readonly' value='" + data.store + "'><br>";
 
-						html += "<label for='store'>Store requested from:</label>"
-								+ "<input type='text' name='store' readonly='readonly' value='" + data[i].store + "'><br>";
+	html += "<label for='food'>Food requested:</label>"
+			+ "<input type='text' name='food' readonly='readonly' value='" + data.food + "'><br>";
 
-						html += "<label for='food'>Food requested:</label>"
-								+ "<input type='text' name='food' readonly='readonly' value='" + data[i].food + "'><br>";
+	html += "<label for='date'>Date made:</label>"
+		 	+ "<input type='text' name='date' readonly='readonly' value='" + data.date + "'><br>";
 
-						html += "<label for='date'>Date made:</label>"
-							 	+ "<input type='text' name='date' readonly='readonly' value='" + data[i].date + "'><br>";
-
-						html += "<button type='submit' class='button button-block'>Accept Order</button><br><br><br>";
-						html += "</form>";
-					}
-				}
-			}
-			displayOrders.html(html);
-
-			// Deliverer accepted an order
-			$(function () {
-			    $('form').on('submit', function (e) {
-
-			    	console.log($(this).find('input[name=id]').val());
-
-					var formData = {
-						orderID: $(this).find('input[name=id]').val(),
-					}
-
-			        $.ajax({
-			            type: 'post',
-			            url: 'update_order',
-			            data: formData,
-			            success: function () {
-			            	alert("Order accepted!");
-			            }
-			        });
-			        e.preventDefault();
-			    });
-			});
-
-			},
-		});
-	}
-	else if (search_orders == "List All"){
-		$.ajax({
-		type: "GET",
-		url: "search_all",
-		data: search_orders,
-		success: function(data, textStatus, jqXHR) {
-			var html = "";
-			// console.log(data);
-			for (i = 0; i < data.length; i++){
-				if (data[i].orderStatus == "Pending"){
-					html += "<form id='" + data[i]._id + "''>";
-					html += "<label for='orderID'>Order ID:</label>" 
-					        + "<input type='text' name='id' readonly='readonly' value='" + data[i]._id + "'><br>";
-
-					html += "<label for='deliveryLocation'>Delivery Location:</label>"
-							+ "<input type='text' name='location' readonly='readonly' value='" + data[i].userLocation + "'><br>";
-
-					html += "<label for='store'>Store requested from:</label>"
-							+ "<input type='text' name='store' readonly='readonly' value='" + data[i].store + "'><br>";
-
-					html += "<label for='food'>Food requested:</label>"
-							+ "<input type='text' name='food' readonly='readonly' value='" + data[i].food + "'><br>";
-
-					html += "<label for='date'>Date made:</label>"
-						 	+ "<input type='text' name='date' readonly='readonly' value='" + data[i].date + "'><br>";
-
-					html += "<button type='submit' class='button button-block'>Accept Order</button><br><br><br>";
-					html += "</form>";
-				}
-			}
-			displayOrders.html(html);
-			
-
-			// Deliverer accepted an order
-			$(function () {
-			    $('form').on('submit', function (e) {
-
-			    	console.log($(this).find('input[name=id]').val());
-					console.log(thisUser);
-					var formData = {
-						orderID: $(this).find('input[name=id]').val(),
-						deliverer: thisUser._id,
-					}
-
-			        $.ajax({
-			            type: 'post',
-			            url: 'update_order',
-			            data: formData,
-			            success: function () {
-			            	alert("Order accepted!");
-			            }
-			        });
-			        e.preventDefault();
-			    });
-			});
-
-
-			},
-		});
-	}
-});
-
-logout.click(function (e) {
-	delete_cookie("loginDeliverer");
-	window.location = "index.html";
-});
-
-// Function that deletes a cookie.
-var delete_cookie = function(name) {
-    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-};
+	html += "<button type='submit' class='button button-block'>Accept Order</button><br><br><br>";
+	html += "</form>";
+	return html;
+}
